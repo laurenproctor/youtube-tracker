0e300c169ffad5e67ef36c4d1245bf2cc1f25042
Duc Le <ducle.sg@gmail.com>
2012-11-10 01:36:05 +0700

    draw chart subscribers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,34 @@ class HomeController < ApplicationController
 
     @top_playlists = DayPlaylistTracker.where(:this_week_rank => 1 .. 25, :tracked_date => today).
       order(sort_column(DayPlaylistTracker) + " " + sort_direction).limit(25)
+    rows = DayChannel.where(:imported_date => Time.now - 7.days .. Time.now)
+    @subscribers = {}
+    @keys = []
+    rows.each do |p|
+      key = p.imported_date.strftime('%m/%d')
+      @subscribers.merge!( key => p.subscribers )
+      @keys << key
+    end
+
+    facebook_info_rows = DayFacebookInfo.where(:imported_date => Time.now - 7.days .. Time.now)
+    @facebook_info_json = {}
+    @facebook_info_keys = []
+    @facebook_likes_json = {}
+    facebook_info_rows.each do |p|
+      key = p.imported_date.strftime('%m/%d')
+      @facebook_info_json.merge!( key => p.talking_about_count )
+      @facebook_likes_json.merge!( key => p.likes )
+      @facebook_info_keys << key
+    end
+
+    twitter_info_rows = DayTwitterInfo.where(:imported_date => Time.now - 7.days .. Time.now)
+    @twitter_info_json = {}
+    @twitter_info_keys = []
+    facebook_info_rows.each do |p|
+      key = p.imported_date.strftime('%m/%d')
+      @twitter_info_json.merge!( key => p.likes )
+      @twitter_info_keys << key
+    end
   end
 end
 
