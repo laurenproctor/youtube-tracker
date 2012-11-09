b831e3c888567efa942b86cbecfe3144129dfc17
Duc Le <ducle.sg@gmail.com>
2012-11-09 16:23:42 +0700

    query top videos,  top playlists by today

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
+    today = Time.now.beginning_of_day
     @channel = Channel.find_by_username(YOUTUBE[:user_id])
-    @top_videos = DayVideoTracker.where(:this_week_rank => 1 .. 25).
+    @top_videos = DayVideoTracker.where(:this_week_rank => 1 .. 25, :tracked_date => today).
       order(sort_column(DayVideoTracker) + " " + sort_direction).limit(25) # .page(params[:page])
 
-    @top_playlists = DayPlaylistTracker.where(:this_week_rank => 1 .. 25).
+    @top_playlists = DayPlaylistTracker.where(:this_week_rank => 1 .. 25, :tracked_date => today).
       order(sort_column(DayPlaylistTracker) + " " + sort_direction).limit(25)
   end
 end
