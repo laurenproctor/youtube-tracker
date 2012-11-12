5a893b1ad4f4f2ec2d797f5374b6f89a14b407c7
Duc Le <ducle.sg@gmail.com>
2012-11-12 10:02:41 +0700

    4 charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,11 @@ class HomeController < ApplicationController
 
     @top_playlists = DayPlaylistTracker.where(:this_week_rank => 1 .. 25, :tracked_date => today).
       order(sort_column(DayPlaylistTracker) + " " + sort_direction).limit(25)
+
     seven_days = Time.now - 7.days .. Time.now
+    last_week_seven_days = Time.now - 14.days .. Time.now - 7.days
     rows = DayChannel.where(:imported_date => seven_days)
+    last_week_rows = DayChannel.where(:imported_date => last_week_seven_days)
     @subscribers = {}
     @keys = []
     rows.each do |p|
@@ -17,16 +20,18 @@ class HomeController < ApplicationController
       @keys << key
     end
 
-    avg_views_rows=DayVideo.where(:imported_date => seven_days).select('imported_date, avg(view_count) as view_count').group(:imported_date)
+    avg_views_rows=DayVideo.where(:imported_date => seven_days).select('imported_date, avg(day_view_count) as day_view_count').group(:imported_date)
+    last_week_avg_views_rows=DayVideo.where(:imported_date => last_week_seven_days).select('imported_date, avg(day_view_count) as day_view_count').group(:imported_date)
     @avg_views_json = {}
     @avg_views_keys = []
     avg_views_rows.each do |p|
       key = p[:imported_date].strftime('%m/%d')
-      @avg_views_json.merge!( key => p[:view_count] )
+      @avg_views_json.merge!( key => p[:day_view_count] )
       @avg_views_keys << key
     end
 
     facebook_info_rows = DayFacebookInfo.where(:imported_date => seven_days)
+    last_week_facebook_info_rows = DayFacebookInfo.where(:imported_date => last_week_seven_days)
     @facebook_info_json = {}
     @facebook_info_keys = []
     @facebook_likes_json = {}
@@ -38,6 +43,7 @@ class HomeController < ApplicationController
     end
 
     twitter_info_rows = DayTwitterInfo.where(:imported_date => seven_days)
+    last_week_twitter_info_rows = DayTwitterInfo.where(:imported_date => last_week_seven_days)
     @twitter_info_json = {}
     @twitter_info_keys = []
     facebook_info_rows.each do |p|
