096093260f534ca1fb0dafe3d66d9c61219bf8f0
Duc Le <ducle.sg@gmail.com>
2012-11-19 16:22:13 +0700

    youtube user_id=officialcomedy

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -1,7 +1,7 @@
 YOUTUBE = {
   :dev_key => 'AI39si4wNpuwLhab21bgqpB7PxF-IJlDHvNLQC0anVc11yzqEQLoouGHtFU1_Q4I_Q5PbjfUbUY12MyCCF73D0Ce2TFtIeO5CQ',
-  # :user_id => 'officialcomedy'
-  :user_id => 'NetworkA'
+  :user_id => 'officialcomedy'
+  # :user_id => 'NetworkA'
 }
 
 FACEBOOK = {
