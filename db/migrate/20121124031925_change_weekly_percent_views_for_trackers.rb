6ee5ac19fc3b638a830e5ffbe46aa9ef8b493f13
Duc Le <ducle.sg@gmail.com>
2012-11-25 06:23:29 +0700

    remove migration change weekly percent views

--- db/migrate/20121124031925_change_weekly_percent_views_for_trackers.rb
+++
@@ -1,8 +0,0 @@
-class ChangeWeeklyPercentViewsForTrackers < ActiveRecord::Migration
-  def change
-    change_table :trackers do |t|
-      t.change :weekly_percent_views, :decimal, :precision => 10, :scale => 4
-    end
-  end
-end
-
