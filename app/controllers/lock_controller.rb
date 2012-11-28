a5cc6d2991b3532c577ffa261f30b954dc6167df
Duc Le <ducle.sg@gmail.com>
2012-11-28 15:29:50 +0700

    auto render login site if did not sign in

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -1,9 +1,15 @@
 class LockController < ApplicationController
 	layout 'landing'
 
+	def refused
+		render :action => 'login'
+
+	end
+
   def unlock
     if Lock.passwords_match?(params[:password])
       session[:lock_opened] = true
+      redirect_to root_path
     else
       redirect_to :action=>:login
     end
